31510728608f6e57fbff68d4b65fc6ea632b79e8
Angel Eduardo Lozano Niño <alozan@users.noreply.github.com>
2022-11-02 10:15:13 -0600

    updates

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-# Update packages list and update system
-sudo apt update
-sudo apt upgrade -y
-
 # Install brave-browser
 sudo apt install apt-transport-https curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -40,3 +36,30 @@ bash ~/Anaconda3-2022.10-Linux-x86_64.sh
 sudo apt-get update
 sudo apt-get install r-base r-base-dev
 sudo apt-get install libatlas3-base
+
+#Install Neovim
+sudo apt install neovim
+sudo apt install python3-neovim
+
+#Install Docker
+sudo apt-get update
+ sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+#run Docker
+
+sudo docker run hello-world
--- prev.sh
+++ prev.sh
@@ -1,7 +1,11 @@
+# Update packages list and update system
 sudo apt-get update
 sudo apt upgrade -y
+
+#Install Git
 sudo apt-get install git
 
+#Clone Repository
 git clone https://github.com/alozan/debian_install.git
 cd debian-install
 sudo ./install.sh
